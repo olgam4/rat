97c4d20fd157341f6abe562664a01ec5f0e8348d
Olivier Gamache <olivier.gamache@radio-canada.ca>
2022-11-27 16:51:08 -0500

    feat: counters

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,60 @@
 use leptos::*;
 
-#[component]
-pub fn SimpleCounter(cx: Scope, initial_value: i32) -> Element {
-    let (value, set_value) = create_signal(cx, initial_value);
-
-    let clear = move |_| set_value(0);
-    let decrement = move |_| set_value.update(|value| *value -= 1);
-    let increment = move |_| set_value.update(|value| *value += 1);
-
-    view! {
-        cx,
-        <div class="flex absolute top-0 bottom-0 right-0 left-0 justify-center items-center space-x-2">
-            <button class="px-4 py-1 rounded bg-blue-200" on:click=clear>"Clear"</button>
-            <button class="px-4 py-1 rounded bg-red-200" on:click=decrement>"-1"</button>
-            <span>"Value: " {move || value().to_string()} "!"</span>
-            <button class="px-4 py-1 rounded bg-green-200" on:click=increment>"+1"</button>
-        </div>
-    }
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+struct Counter {
+    id: usize,
+    count: RwSignal<i32>,
 }
 
+#[component]
+fn Counters(cx: Scope) -> Element {
+    let (counters, set_counters) = create_signal::<Vec<Counter>>(
+        cx,
+        vec![Counter {
+            id: 0,
+            count: create_rw_signal(cx, 0),
+        }],
+    );
+
+    let add_counter = move |_| {
+        set_counters.update(|counters| {
+            let id = counters.len();
+            counters.push(Counter {
+                id,
+                count: create_rw_signal(cx, 0),
+            });
+        })
+    };
+
+    view! {
+      cx,
+      <div class="flex flex-col">
+        <For
+          each=counters
+          key=|counter| counter.id
+        >
+          {|cx: Scope, counter: &Counter| {
+            let count = counter.count;
+            view! {
+              cx,
+              <div class="w-full flex justify-center space-x-2">
+                  <p>"Value: " {move || count.get()}</p>
+                  <button
+                    on:click=move |_| count.update(|count| *count += 1)
+                    >
+                        "Increment"
+                    </button>
+              </div>
+            }
+          }
+        }
+        </For>
+        <button on:click=add_counter>
+            "+"
+        </button>
+      </div>
+    }
+}
 pub fn main() {
-    mount_to_body(|cx| view! { cx, <SimpleCounter initial_value=3 /> })
+    mount_to_body(|cx| view! { cx, <Counters /> })
 }
